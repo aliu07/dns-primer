38678da65c48fc602808cf093577d62649b6fcf7
Alexander Liu <alexander_l_22@hotmail.com>
2024-10-15 21:53:39 -0400

    Add parsing for additional section records

--- dnsClient.py
+++ dnsClient.py
@@ -86,6 +86,16 @@ class dnsClient:
             return
         elif RCODE == 5:
             print("ERROR\tRefused: the name server refuses to perform the requested operation for policy reasons.")
+            return
+
+        # Init offset to 12 to skip header
+        offset = 12
+        # Skip QNAME section
+        while response[offset] != 0:
+            offset += 1
+        # Skip 0x00 label, QTYPE, and QCLASS
+        offset += 5
+        # Offset at this line is index of first byte of answer section (skipped header and question section)
 
         if ANCOUNT == 0:
             print("NOTFOUND")
@@ -93,72 +103,16 @@ class dnsClient:
             print("ERROR\tUnexpected response: record count is negative.")
         else:
             print(f"*** Answer Section ({ANCOUNT} records) ***")
-            # Init offset to 12 to skip header
-            offset = 12
-            # Skip QNAME section
-            while response[offset] != 0:
-                offset += 1
-            # Skip 0x00 label, QTYPE, and QCLASS
-            offset += 5
-                
-            # Extracting all answer section records
-            for i in range(ANCOUNT):
-                length = response[offset]
-                # First 2 bits are '11' indicate that it is a pointer
-                if length & 0xC0 == 0xC0:
-                    offset += 2
-                else:
-                    # Skip NAME
-                    while response[offset] != 0:
-                        offset += 1
-                    # Skip 0x00 label
-                    offset += 1
-                
-                # RESPONSE ANSWER SECTION
-                TYPE = int.from_bytes(response[offset:offset + 2], byteorder='big')
-                offset += 2
-                CLASS = int.from_bytes(response[offset:offset + 2], byteorder='big')
-                CLASS = 5
-                offset += 2
-                TTL = int.from_bytes(response[offset: offset + 4], byteorder='big')
-                offset += 4
-                RDLENGTH = int.from_bytes(response[offset: offset + 2], byteorder='big')
-                offset += 2
-
-                # VALIDATION
-                if CLASS != 1:
-                    print(f"ERROR\tUnexpected response: CLASS value should be 1, but is {CLASS}.")
-                    continue
-
-                if TYPE == 0x0001:
-                    # If we do not have 4 octets, we do not have a valid IP address
-                    if RDLENGTH != 4:
-                        print("ERROR\tInvalid IP address field in answer record.")
-                        continue
-                    # Build IP address
-                    IP_ADDRESS = ".".join(str(label) for label in response[offset:offset + 4])
-                    print(f"IP\t{IP_ADDRESS}\t{TTL}\t{AUTH}")
-
-                elif TYPE == 0x0002:
-                    ALIAS = self.parse_alias(response, offset)
-                    print(f"NS\t{ALIAS}\t{TTL}\t{AUTH}")
-
-                elif TYPE == 0x005:
-                    ALIAS = self.parse_alias(response, offset)
-                    print(f"CNAME\t{ALIAS}\t{TTL}\t{AUTH}")
-
-                elif TYPE == 0x000f:
-                    PREFERENCE = int.from_bytes(response[offset:offset + 2], byteorder='big')
-                    ALIAS = self.parse_alias(response, offset + 2)
-                    print(f"MX\t{ALIAS}\t{PREFERENCE}\t{TTL}\t{AUTH}")
-
-                else:
-                    print(f"ERROR\tUnsupported record type found in answer section: {TYPE}.")
-
-                offset += RDLENGTH
+            # Extracting all answer section records    
+            offset = self.parse_records(response, ANCOUNT, offset, AUTH)
+        
+        # Skip authority section
+        offset = self.skip_authority_section(response, NSCOUNT, offset)
 
         if ARCOUNT > 0:
             print(f"*** Additional Section ({ARCOUNT} records) ***")
+            # Extracting all additional section records
+            offset = self.parse_records(response, ARCOUNT, offset, AUTH)
 
     def parse_alias(self, response, offset):
         labels = []
@@ -188,12 +142,95 @@ class dnsClient:
 
         return ".".join(labels)
 
+    def parse_records(self, response, num_records, offset, AUTH):
+        for i in range(num_records):
+            length = response[offset]
+            # First 2 bits are '11' indicate that it is a pointer
+            if length & 0xC0 == 0xC0:
+                offset += 2
+            else:
+                # Skip NAME
+                while response[offset] != 0:
+                    offset += 1
+                # Skip 0x00 label
+                offset += 1
+            
+            # RESPONSE ANSWER SECTION
+            TYPE = int.from_bytes(response[offset:offset + 2], byteorder='big')
+            offset += 2
+            CLASS = int.from_bytes(response[offset:offset + 2], byteorder='big')
+            offset += 2
+            TTL = int.from_bytes(response[offset: offset + 4], byteorder='big')
+            offset += 4
+            RDLENGTH = int.from_bytes(response[offset: offset + 2], byteorder='big')
+            offset += 2
+
+            # VALIDATION
+            if CLASS != 1:
+                print(f"ERROR\tUnexpected response: CLASS value should be 1, but is {CLASS}.")
+                continue
+
+            if TYPE == 0x0001:
+                # If we do not have 4 octets, we do not have a valid IP address
+                if RDLENGTH != 4:
+                    print("ERROR\tInvalid IP address field in answer record.")
+                    continue
+                # Build IP address
+                IP_ADDRESS = ".".join(str(label) for label in response[offset:offset + 4])
+                print(f"IP\t{IP_ADDRESS}\t{TTL}\t{AUTH}")
+
+            elif TYPE == 0x0002:
+                ALIAS = self.parse_alias(response, offset)
+                print(f"NS\t{ALIAS}\t{TTL}\t{AUTH}")
+
+            elif TYPE == 0x005:
+                ALIAS = self.parse_alias(response, offset)
+                print(f"CNAME\t{ALIAS}\t{TTL}\t{AUTH}")
+
+            elif TYPE == 0x000f:
+                PREFERENCE = int.from_bytes(response[offset:offset + 2], byteorder='big')
+                ALIAS = self.parse_alias(response, offset + 2)
+                print(f"MX\t{ALIAS}\t{PREFERENCE}\t{TTL}\t{AUTH}")
+
+            else:
+                print(f"ERROR\tUnsupported record type found in answer section: {TYPE}.")
+
+            offset += RDLENGTH
+
+        return offset
+
+    def skip_authority_section(self, response, num_records, offset):
+        for i in range(num_records):
+            # Skip the name field
+            while True:
+                length = packet[offset]
+                if length == 0:
+                    offset += 1
+                    break
+                elif length & 0xC0 == 0xC0:
+                    offset += 2
+                    break
+                else:
+                    offset += length + 1
+
+            # Skip Type (2 bytes), Class (2 bytes), TTL (4 bytes), and RDLength (2 bytes)
+            offset += 10
+
+            # Read RDLength
+            rdlength = struct.unpack('>H', packet[offset-2:offset])[0]
+
+            # Skip the RDATA field
+            offset += rdlength
+
+        return offset  # This is now the start of the additional section
+
+
 
 
 if __name__ == "__main__":
     # Type A Query: python3 dnsClient.py -t 10 -r 3 @8.8.8.8 www.mcgill.ca
     # Type MX Query: python3 dnsClient.py -t 10 -r 2 -mx @8.8.8.8 mcgill.ca
     # Type NS Query: python3 dnsClient.py -ns @8.8.8.8 mcgill.ca
-    # python3 dnsClient.py -mx @8.8.8.8 gmail.com
+    # Type MX Query: python3 dnsClient.py -mx @8.8.8.8 gmail.com
     dnsClient = dnsClient()
     dnsClient.main()
